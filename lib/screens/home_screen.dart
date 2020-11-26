0161a550c7e6a60310af054f3c96a1bc7ed1c375
sannidhya-kushwaha <sannidhyakushwaha@gmail.com>
2020-11-26 10:02:38 +0530

    Layout Builder - Remaining Money Bars

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:Budget_App/data/data.dart';
+import 'package:Budget_App/helpers/color_helper.dart';
 import 'package:Budget_App/models/category_model.dart';
 import 'package:Budget_App/models/expense_model.dart';
 import 'package:Budget_App/widgets/bar_chart.dart';
@@ -13,10 +14,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   _buildCategory(Category category, double totalAmountSpent) {
     return Container(
-      margin: EdgeInsets.symmetric(
-        horizontal: 20.0,
-        vertical: 10.0,
-      ),
+      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      padding: EdgeInsets.all(20.0),
       height: 100.0,
       width: double.infinity,
       decoration: BoxDecoration(
@@ -39,19 +38,51 @@ class _HomeScreenState extends State<HomeScreen> {
               Text(
                 category.name,
                 style: TextStyle(
-                  fontSize: 20.0,
+                  fontSize: 16.0,
                   fontWeight: FontWeight.w600,
                 ),
               ),
               Text(
                 '\$${(category.maxAmount - totalAmountSpent).toStringAsFixed(2)} / \$${category.maxAmount.toStringAsFixed(2)}',
                 style: TextStyle(
-                  fontSize: 18.0,
+                  fontSize: 14.0,
                   fontWeight: FontWeight.w600,
                 ),
               ),
             ],
           ),
+          SizedBox(height: 10.0),
+          LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              final double maxBarWidth = constraints.maxWidth;
+              final double percent =
+                  (category.maxAmount - totalAmountSpent) / category.maxAmount;
+              double barWidth = percent * maxBarWidth;
+
+              if (barWidth < 0) {
+                barWidth = 0;
+              }
+              return Stack(
+                children: [
+                  Container(
+                    height: 20.0,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[200],
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                  Container(
+                    height: 20.0,
+                    width: barWidth,
+                    decoration: BoxDecoration(
+                      color: getColor(context, percent),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  )
+                ],
+              );
+            },
+          )
         ],
       ),
     );
